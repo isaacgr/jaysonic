5ad22eaa1a3898f68f12977ee9ef438171933f82
isaacgr <isaacgrowell@gmail.com>
2019-05-17 15:33:05 -0400

    Initialize repo

---
+++ tests/client.test.js
@@ -0,0 +1,9 @@
+const Jasonic = require("../");
+
+const client = new Jasonic.client({ host: "172.16.230.4", port: 6969 });
+
+client.connect().then(conn => {
+  console.log(
+    `Client connected to ${client.server.host} on port ${client.server.port}`
+  );
+});
