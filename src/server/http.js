475fab47a5c5928d08acf71db4eff1218fbd5ffe
isaacgr <isaacgrowell@gmail.com>
2019-06-27 17:29:39 -0400

    Fix http server data handling

--- src/server/http.js
+++ src/server/http.js
@@ -33,98 +33,62 @@ class HTTPServer extends Server {
           this.messageBuffer += data;
         });
         request.on("end", () => {
-          /**
-           * HTTP messages could contain delimited requests
-           * or be batched.
-           *
-           * Delimited requests should be resolved, and then sent back
-           * as a delimited response
-           *
-           * Batched requests get handled individually, results wrapped in a list
-           * and then sent back to client
-           */
           const messages = this.messageBuffer.split(this.options.delimiter);
           this.messageBuffer = "";
-          if (messages.length > 1) {
-            // delimited request
-            const promises = messages
-              .filter(messageString => messageString !== "")
-              .map(message => this.validateRequest(message)
-                .then(({ json }) => this.getResult(json)
-                  .then((result) => {
-                    this.setResponseHeader(response);
-                    return result;
+          try {
+            messages
+              .filter((messageString) => messageString !== "")
+              .map((chunk) => {
+                const validRequest = () =>
+                  this.validateRequest(chunk)
+                    .then((result) => result)
+                    .catch((error) => {
+                      throw new Error(JSON.stringify(error));
+                    });
+                validRequest()
+                  .then((message) => {
+                    if (message.batch) {
+                      this.setResponseHeader(response);
+                      return response.write(
+                        JSON.stringify(message.batch),
+                        () => {
+                          response.end();
+                        }
+                      );
+                    }
+                    this.getResult(message.json)
+                      .then((json) => {
+                        this.setResponseHeader(response);
+                        return response.write(json, () => {
+                          response.end();
+                        });
+                      })
+                      .catch((error) => {
+                        this.setResponseHeader(response, error.error.code);
+                        return response.write(JSON.stringify(error), () => {
+                          response.end();
+                        });
+                      });
                   })
-                  .catch((error) => {
-                    this.setResponseHeader(response, error.error.code);
-                    return JSON.stringify(error);
-                  }))
-                .catch((error) => {
-                  this.setResponseHeader(response, error.error.code);
-                  return JSON.stringify(error);
-                }));
-            Promise.all(promises)
-              .then((result) => {
-                const res = result.join(this.options.delimiter);
-                response.write(res, () => {
-                  response.end();
-                });
-              })
-              .catch((error) => {
-                const res = error.join(this.options.delimiter);
-                response.write(res, () => {
-                  response.end();
-                });
+                  .catch((error) =>
+                    response.write(error.message, () => {
+                      response.end();
+                    })
+                  );
               });
-          } else {
-            // possibly a batch request, check and resolve if so
-            // reject otherwise
-            try {
-              const message = JSON.parse(messages);
-              if (!_.isArray(message)) {
-                throw new SyntaxError();
-              }
-            } catch (e) {
-              const error = this.sendError(
-                null,
-                ERR_CODES.parseError,
-                ERR_MSGS.parseError
-              );
-              this.setResponseHeader(response, error.code);
-              return response.write(
-                JSON.stringify(error) + this.options.delimiter,
-                () => {
-                  response.end();
-                }
-              );
-            }
-            if (
-              _.isArray(JSON.parse(messages))
-              && _.isEmpty(JSON.parse(messages))
-            ) {
-              const error = this.sendError(
-                null,
-                ERR_CODES.invalidRequest,
-                ERR_MSGS.invalidRequest
-              );
-              this.setResponseHeader(response, error.code);
-              return response.write(JSON.stringify([error]), () => {
+          } catch (e) {
+            const error = this.sendError(
+              null,
+              ERR_CODES.parseError,
+              ERR_MSGS.parseError
+            );
+            this.setResponseHeader(response, error.code);
+            return response.write(
+              JSON.stringify(error) + this.options.delimiter,
+              () => {
                 response.end();
-              });
-            }
-            this.handleBatchRequest(messages)
-              .then((responses) => {
-                const res = JSON.stringify(responses);
-                response.write(res, () => {
-                  response.end();
-                });
-              })
-              .catch((error) => {
-                const res = JSON.stringify(error);
-                response.write(res, () => {
-                  response.end();
-                });
-              });
+              }
+            );
           }
         });
       });
@@ -161,7 +125,7 @@ class HTTPServer extends Server {
 
   clientDisconnected(cb) {
     this.on("clientDisconnected", (client) => {
-      const clientIndex = this.connectedClients.findIndex(c => client === c);
+      const clientIndex = this.connectedClients.findIndex((c) => client === c);
       if (clientIndex === -1) {
         return "unknown";
       }
--- tests/client/http-client.test.js
+++ tests/client/http-client.test.js
@@ -1,15 +1,67 @@
-// const Jaysonic = require("../../src");
+const { expect } = require("chai");
+const Jaysonic = require("../../src");
 
-// cosnt {serverhttp} = require('../test-server')
-// const clienthttp = new Jaysonic.client.http({ port: 8800 });
+const { serverHttp } = require("../test-server");
+const clienthttp = new Jaysonic.client.http({ port: 8800 });
 
-// describe('HTTP Client', () => {
-//   describe('connection', () => {
+before((done) => {
+  serverHttp.listen().then(() => {
+    done();
+  });
+});
 
-//   })
-//   describe('requests', () => {
-//     it('should get response for positional params', (done) => {
-
-//     })
-//   })
-// })
+describe("HTTP Client", () => {
+  describe("connection", () => {});
+  describe("requests", () => {
+    it("should get a response for positional params", (done) => {
+      const request = clienthttp.request().send("add", [1, 2]);
+      request.then((response) => {
+        expect(response.body).to.be.eql({
+          result: 3,
+          jsonrpc: "2.0",
+          id: 1
+        });
+        done();
+      });
+    });
+    it("should get a response for named params", (done) => {
+      const request = clienthttp.request().send("greeting", { name: "Isaac" });
+      request.then((response) => {
+        expect(response.body).to.be.eql({
+          result: "Hello Isaac",
+          jsonrpc: "2.0",
+          id: 2
+        });
+        done();
+      });
+    });
+    it("should get an 'invalid params' error", (done) => {
+      const request = clienthttp.request().send("add", {});
+      request.catch((response) => {
+        expect(response.body).to.be.eql({
+          jsonrpc: "2.0",
+          error: {
+            code: -32602,
+            message: "Invalid Parameters"
+          },
+          id: 3
+        });
+        done();
+      });
+    });
+    it("should get a 'method not found' error", (done) => {
+      const request = clienthttp.request().send("nonexistent", {});
+      request.catch((response) => {
+        expect(response.body).to.be.eql({
+          jsonrpc: "2.0",
+          error: {
+            code: -32601,
+            message: "Method not found"
+          },
+          id: 4
+        });
+        done();
+      });
+    });
+  });
+});
