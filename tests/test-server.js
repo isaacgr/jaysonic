2e933636025ce24969a2dae9c06573a45d949b7a
isaacgr <isaacgrowell@gmail.com>
2019-07-10 07:35:14 -0400

    Add websocket server to test-server.js.

--- tests/test-server.js
+++ tests/test-server.js
@@ -7,12 +7,13 @@ const serverV1 = new Jaysonic.server.tcp({
   port: 8600,
   version: 1
 });
-
 const serverHttp = new Jaysonic.server.http({
   host: "127.0.0.1",
   port: 8800
 });
+const wss = new Jaysonic.server.ws();
 
+// TCP Server
 server.method("add", ([a, b]) => a + b);
 server.method("greeting", ({ name }) => `Hello ${name}`);
 server.method("typeerror", ([a]) => {
@@ -21,8 +22,10 @@ server.method("typeerror", ([a]) => {
   }
 });
 server.method("large.data", () => data);
-serverHttp.method("large.data", () => data);
+server.onNotify("noitify", () => "notification");
 
+// HTTP Server
+serverHttp.method("large.data", () => data);
 serverHttp.method("add", ([a, b]) => a + b);
 serverHttp.method("greeting", ({ name }) => `Hello ${name}`);
 serverHttp.method("typeerror", ([a]) => {
@@ -31,8 +34,20 @@ serverHttp.method("typeerror", ([a]) => {
   }
 });
 
+// Websocket server
+wss.method("add", ([a, b]) => a + b);
+wss.method("greeting", ({ name }) => `Hello ${name}`);
+wss.method("typeerror", ([a]) => {
+  if (typeof a === "number") {
+    throw new TypeError();
+  }
+});
+wss.method("large.data", () => data);
+wss.onNotify("noitify", () => "notification");
+
+// V1 TCP Server
 serverV1.method("add", ([a, b]) => a + b);
 
-server.onNotify("noitify", () => "notification");
-
-module.exports = { server, serverHttp, serverV1 };
+module.exports = {
+  server, serverHttp, serverV1, wss
+};
