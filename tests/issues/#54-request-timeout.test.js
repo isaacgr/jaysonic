2e05807d4b0a24bf435d4afd1b026f0d59d4e058
isaacgr <isaacgrowell@gmail.com>
2020-02-16 16:14:27 -0500

    Add web client to tests

--- tests/issues/#54-request-timeout.test.js
+++ tests/issues/#54-request-timeout.test.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const Jaysonic = require("../../src");
+const JaysonicWebClient = require("../../src/client-ws");
 
 const tcpserver = new Jaysonic.server.tcp({ port: 9997 });
 const httpserver = new Jaysonic.server.http({ port: 9996 });
@@ -8,6 +9,10 @@ const wss = new Jaysonic.server.ws({ port: 6665 });
 const tcpclient = new Jaysonic.client.tcp({ port: 9997, timeout: 0 });
 const httpclient = new Jaysonic.client.http({ port: 9996, timeout: 0 });
 const wsclient = new Jaysonic.client.ws({
+  url: "ws://127.0.0.1:6665",
+  timeout: 0
+});
+const wsweb = new JaysonicWebClient.wsclient({
   url: "ws://127.0.0.1:6665",
   timeout: 0
 });
@@ -56,6 +61,23 @@ describe("#54 Request timeout", () => {
               done();
             });
         });
+      });
+    });
+  });
+  describe("ws web", () => {
+    it("should return object for request timeout", (done) => {
+      wsweb.connect().then(() => {
+        wsweb
+          .request()
+          .send("timeout")
+          .catch((error) => {
+            expect(error).to.be.eql({
+              jsonrpc: "2.0",
+              error: { code: -32000, message: "Request Timeout" },
+              id: null
+            });
+            done();
+          });
       });
     });
   });
