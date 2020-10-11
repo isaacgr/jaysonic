312fc399b4e27d38aa78f6eea2248754d0c054b6
isaacgr <isaacgrowell@gmail.com>
2020-10-11 18:27:44 -0400

    Initial jsdoc stuff. Refs #78.

--- src/client/protocol/tcp.js
+++ src/client/protocol/tcp.js
@@ -1,7 +1,18 @@
 const net = require("net");
 const JsonRpcClientProtocol = require("./base");
 
+/**
+ * Creates an instance of TcpClientProtocl
+ *
+ * @extends JsonRpcClientProtocol
+ */
 class TcpClientProtocol extends JsonRpcClientProtocol {
+  /**
+   * Set the `connector` attribute for the protocol instance. The connector is essentially the
+   * socket instance for the client.
+   *
+   * For the TcpClientProtocl this is `net.Socket()`
+   */
   setConnector() {
     this.connector = new net.Socket();
   }
--- src/client/protocol/ws-browser.js
+++ src/client/protocol/ws-browser.js
@@ -1,11 +1,22 @@
 const WsClientProtocol = require("./ws");
 
+/**
+ * Creates an instance of WsBrowserClientProtocol
+ * @extends WsClientProtocol
+ */
 class WsBrowserClientProtocol extends WsClientProtocol {
+  /**
+   * Set the `connector` attribute for the protocol instance. The connector is essentially the
+   * socket instance for the client.
+   *
+   * For the WsBrowserClientProtocol this is `window.WebSocket()`
+   */
   setConnector() {
     const { protocols } = this.factory.options;
     this.connector = new window.WebSocket(this.url, protocols);
   }
 
+  /**@inheritdoc */
   handleBatch(message) {
     // check if any requests are notifications
     message.forEach((res) => {
@@ -18,6 +29,7 @@ class WsBrowserClientProtocol extends WsClientProtocol {
     this.gotBatchResponse(message);
   }
 
+  /**@inheritdoc */
   handleNotification(message) {
     this.factory.dispatchEvent(
       new CustomEvent(message.method, { detail: message })
