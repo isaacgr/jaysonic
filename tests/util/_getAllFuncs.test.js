4bdaf5210f075ad2b8aab774944cf7c87c44cd95
isaacgr <isaacgrowell@gmail.com>
2020-11-16 07:35:53 -0500

    Test _getAllFuncs()

---
+++ tests/util/_getAllFuncs.test.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai");
+const JsonRpcServerFactory = require("../../src/server");
+
+class TestFactory extends JsonRpcServerFactory {
+  handleTest() {}
+  test() {}
+}
+
+const test = new TestFactory();
+
+describe("_getAllFuncs", () => {
+  it('should return a list of all server class methods which start with "handle"', (done) => {
+    expect(test._getAllFuncs(test)).to.be.an("array");
+    expect(test._getAllFuncs(test).length).to.eql(1);
+    expect(test._getAllFuncs(test)[0]).to.eql("handleTest");
+    done();
+  });
+});
