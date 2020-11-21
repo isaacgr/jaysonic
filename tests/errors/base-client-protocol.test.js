d75f556fa1a83cd739fa383249d2b6c409ae8430
isaacgr <isaacgrowell@gmail.com>
2020-11-21 10:39:50 -0500

    More error tests for base client protocol

--- tests/errors/base-client-protocol.test.js
+++ tests/errors/base-client-protocol.test.js
@@ -1,7 +1,12 @@
 const { expect } = require("chai");
 const intercept = require("intercept-stdout");
+const chai = require("chai");
+const spies = require("chai-spies");
 const JsonRpcClientProtocol = require("../../src/client/protocol/base");
 const JsonRpcClientFactory = require("../../src/client/");
+const { formatError } = require("../../src/util/format");
+
+chai.use(spies);
 
 const factory = new JsonRpcClientFactory({});
 factory.requestTimeout = 100;
@@ -63,5 +68,48 @@ describe("Base Client Protocol Errors", () => {
       );
       done();
     }, 100);
+  });
+  it("should call rejectPendingCalls with an unknown error if gotError cannot be parsed", (done) => {
+    const base = new JsonRpcClientProtocol({}, 2, "\n");
+    const callback = chai.spy.on(base, "rejectPendingCalls");
+    base.gotError("foo");
+    expect(callback).to.have.been.called.with(
+      JSON.parse(
+        formatError({
+          jsonrpc: 2,
+          id: null,
+          code: -32001,
+          message: JSON.stringify("foo", Object.getOwnPropertyNames("foo")),
+          delimiter: "\n"
+        })
+      )
+    );
+    done();
+  });
+  describe("verifyData", () => {
+    it("should raise a syntax error if the message is not an object", (done) => {
+      const chunk = 1;
+      const base = new JsonRpcClientProtocol({}, 2, "\n");
+      try {
+        base.verifyData(chunk);
+      } catch (e) {
+        expect(e.message).to.eql(
+          "{\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32700,\"message\":\"Unable to parse message: '1'\"},\"id\":null}\n"
+        );
+      }
+      done();
+    });
+    it("should raise an error if message has no error or response member, but has an id", (done) => {
+      const chunk = "{ \"id\": 1 }";
+      const base = new JsonRpcClientProtocol({}, 2, "\n");
+      try {
+        base.verifyData(chunk);
+      } catch (e) {
+        expect(e.message).to.eql(
+          "{\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32001,\"message\":\"Unknown Error\"},\"id\":null}\n"
+        );
+      }
+      done();
+    });
   });
 });
