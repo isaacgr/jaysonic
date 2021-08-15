49acacf6a02ac73b087de47ce5d472d9dba171e1
isaac <isaacgrowell@gmail.com>
2021-08-15 10:18:15 -0400

    Use httpbin for notification test

--- tests/notifications_and_subscriptions/http-client-protocol.test.js
+++ tests/notifications_and_subscriptions/http-client-protocol.test.js
@@ -2,8 +2,8 @@ const { expect } = require("chai");
 const Jaysonic = require("../../src/");
 
 const http = new Jaysonic.client.http({
-  host: "gurujsonrpc.appspot.com",
-  path: "/guru",
+  host: "httpbin.org",
+  path: "/status/200",
   scheme: "https",
   port: 443
 });
@@ -12,7 +12,7 @@ describe("Http Client Protocol Notifications", () => {
   it("should reject with error if no 204 response recieved for notification", (done) => {
     http
       .request()
-      .notify("guru.test", ["isaac"])
+      .notify("guru.test")
       .catch((error) => {
         expect(error.message).to.eql("no response receieved for notification");
         done();
