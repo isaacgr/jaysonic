9e82f51e10b96a490a6cdc62b9cf696ad5750824
isaac <isaacgrowell@gmail.com>
2021-08-22 16:44:24 -0400

    Add test for infinite retries. Refs #94.

--- tests/connections/base-client-protocol.test.js
+++ tests/connections/base-client-protocol.test.js
@@ -3,6 +3,7 @@ const intercept = require("intercept-stdout");
 const Jaysonic = require("../../src");
 
 const client = new Jaysonic.client.tcp({ retries: 1 });
+const infClient = new Jaysonic.client.tcp({ retries: Infinity });
 
 describe("Base Client Reconnect", () => {
   it("should retry the connection to the server and log the attempts", (done) => {
@@ -14,11 +15,26 @@ describe("Base Client Reconnect", () => {
     setTimeout(() => {
       unhook();
       expect(capturedText).to.equal(
-        "Unable to connect. Retrying. 0 attempts left.\n"
+        "Failed to connect. Address [127.0.0.1:8100]. Retrying. 0 attempts left.\n"
       );
     }, 100);
     conn.catch(() => {
       done();
     });
+  }).timeout(10000);
+  it("should retry the connection to the server indefinitely if retries set to 'Infinity'", (done) => {
+    infClient.connect();
+    let capturedText = "";
+    const unhook = intercept((text) => {
+      capturedText += text;
+    });
+    setTimeout(() => {
+      unhook();
+      expect(capturedText).to.equal(
+        "Failed to connect. Address [127.0.0.1:8100]. Retrying.\n"
+      );
+      infClient.end();
+      done();
+    }, 100);
   }).timeout(10000);
 });
