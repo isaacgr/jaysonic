02224328f887ea27faf20280671d160c6cedd37b
isaac <isaacgrowell@gmail.com>
2021-08-22 19:39:34 -0400

    Add ws client tests for indefinite retry

--- tests/connections/ws-client-protocol.test.js
+++ tests/connections/ws-client-protocol.test.js
@@ -4,6 +4,7 @@ const Jaysonic = require("../../src");
 const { wss } = require("../test-server");
 
 const wsClient = new Jaysonic.client.ws({ retries: 1 });
+const infClient = new Jaysonic.client.ws({ retries: Infinity });
 
 describe("WS Client Reconnect", () => {
   it("should attempt to reconnect to the server and reject promise if unable", (done) => {
@@ -12,6 +13,38 @@ describe("WS Client Reconnect", () => {
       expect(error).to.be.an("object");
       done();
     });
+  }).timeout(10000);
+  it("should retry the connection to the server indefinitely if retries set to 'Infinity'", (done) => {
+    infClient.connect();
+    let capturedText = "";
+    const unhook = intercept((text) => {
+      capturedText += text;
+    });
+    setTimeout(() => {
+      unhook();
+      expect(capturedText).to.equal(
+        "Failed to connect. Address [ws://127.0.0.1:8100]. Retrying.\n"
+      );
+      infClient.end();
+      done();
+    }, 100);
+  }).timeout(10000);
+  it("should clear the _connectionTimeout of the protocol instance when connection ended", (done) => {
+    infClient.connect();
+    let capturedText = "";
+    const unhook = intercept((text) => {
+      capturedText += text;
+    });
+    setTimeout(() => {
+      unhook();
+      expect(capturedText).to.equal(
+        "Failed to connect. Address [ws://127.0.0.1:8100]. Retrying.\n"
+      );
+      const pcolConnectionTimeout = infClient.pcolInstance._connectionTimeout;
+      infClient.end();
+      expect(pcolConnectionTimeout._destroyed).to.equal(true);
+      done();
+    }, 1000);
   }).timeout(10000);
 });
 
