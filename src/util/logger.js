67df1dc02626a588522601896880459259e52085
isaac <isaacgrowell@gmail.com>
2021-10-02 17:23:25 -0400

    Add documentation for logger. Refs #43.

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,4 +1,9 @@
 class Logger {
+  /**
+   * Singleton logging class to provide custom logger for library
+   *
+   * @returns instance of Logger
+   */
   constructor() {
     if (Logger._instance) {
       return Logger._instance;
@@ -17,12 +22,28 @@ class Logger {
 }
 
 const logging = {
+  /**
+   * Singleton logging class to provide custom logger for library
+   *
+   * @returns instance of Logger
+   */
   setLogger(newLogger) {
     new Logger().setLogger(newLogger);
   },
+  /**
+   * Overwrite Logger.log, default `console`, which is used for
+   * messages within the library
+   *
+   * @param {object} newLogger Object to overwrite `console` logging
+   */
   getLogger() {
     return new Logger().getLogger().log;
   },
+  /**
+   * Get `Logger` instance
+   *
+   * @returns instance of Logger
+   */
   getInstance() {
     return new Logger().getLogger();
   }
